5065dd0ea946c51016df49705477100ac5233020
Jamal Emmanuel <jamal.emmanuel23@gmail.com>
2022-03-12 13:22:56 -0400

    another change

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Github with heroku/i);
   expect(linkElement).toBeInTheDocument();
 });
